815fba1485325af22f58089b53f49dbc5a28ac12
Andrey Alexeev <andreyalieksieiev@gmail.com>
2019-09-23 23:05:20 +0300

    page Dialogs

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 const App = () => {
     return (
         <div className="app-wrapper">
-
             <Header />
             <Navbar />
-            <Profile />
-            
+            <div className="app-wrapper-content">
+                <Dialogs />
+            </div>
+            {/* <Profile /> */}
         </div>
     )
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className={classes.content}>
+        <div>
             <div>
                 <img src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt=""/>
             </div>
