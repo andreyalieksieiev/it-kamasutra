1ea417b60ed51dec7c70c26834aeea477a683372
Andrey Alexeev <andreyalieksieiev@gmail.com>
2019-09-25 20:51:55 +0300

    21 - Layout of dialogs

--- src/App.js
+++ src/App.js
@@ -24,3 +24,4 @@ const App = () => {
 }
 
 export default App;
+
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Dimych
+                </div>
+                <div className={classes.dialog}>
+                    Andrey
+                </div>
+                <div className={classes.dialog}>
+                    Sveta
+                </div>
+                <div className={classes.dialog}>
+                    Sasha
+                </div>
+                <div className={classes.dialog}>
+                    Viktor
+                </div>
+                <div className={classes.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How is your it-kamasutra</div>
+                <div className={classes.message}>Yo</div>
+            </div>
         </div>
     ) 
 }
