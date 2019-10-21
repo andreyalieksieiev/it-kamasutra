70bf17539990361e897aa08cac273e7b734a7a3d
Andrey Alexeev <andreyalieksieiev@gmail.com>
2019-10-21 23:24:41 +0300

    52 - Side Effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,14 +5,17 @@ import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
 
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items);
-        });
-    } 
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items);
+            });
+        } 
+    }
     
     return <div>
+        <button onClick={getUsers} >Get Users</button>
         {
         props.users.map(u => <div key={u.id} >
             <span>
