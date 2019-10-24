696baacf6df5243b6c1b7e5e7ba827979f30e41b
Andrey Alexeev <andreyalieksieiev@gmail.com>
2019-10-24 23:45:59 +0300

    54 - componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,11 +4,11 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png';
 
 class Users extends React.Component{
-    constructor(props){
-        super(props);
+
+    componentDidMount(){
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    this.props.setUsers(response.data.items);
+            .then(response => {
+                this.props.setUsers(response.data.items);
             });
     }
 
